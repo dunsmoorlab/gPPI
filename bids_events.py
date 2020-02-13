f79e29e6b8574f1e4134cadb189ffe25ea26d6c2
achennings <achennings@utexas.edu>
2020-02-13 14:22:45 -0600

    manual

--- bids_events.py
+++ bids_events.py
@@ -13,26 +13,30 @@ class bids_events():
 
     def CS_fsl(self): #generate CS+, CS-, US timing files for use in FSL
 
-        outstr = {'CS+':'CSp',
-                  'CS-':'CSm'}
+        outstr = {'CS+'      :'CSp',
+                  'CS-'      :'CSm',
+                  'animal'   :'Animal',
+                  'tool'     :'Tool',
+                  'indoor'   :'Indoor',
+                  'outdoor'  :'Outdoor',
+                  'scrambled':'Scrambled',
+                  'rest'     :'Rest'}
 
-        for folder in os.walk(subj.subj_dir):
+        for folder in os.walk(self.subj.subj_dir):
             for file in folder[2]:
                 if 'events' in file and '.tsv' in file:
-                    events = pd.read_csv(os.path.join(subj.subj_dir,folder[0],file), sep='\t')
+                    events = pd.read_csv(os.path.join(self.subj.subj_dir,folder[0],file), sep='\t')
                     phase = re.search('task-(.*)_events',file)[1]
-                    out = os.path.join(subj.model_dir,'task-%s'%(phase))
+                    out = os.path.join(self.subj.model_dir,'task-%s'%(phase))
                     mkdir(out)
 
-                    if 'localizer' not in phase:
-                        for con in events.trial_type.unique():
-                            con_timing = events[events.trial_type == con][['onset','duration']]
-                            con_timing['PM'] = 1
-                            con_timing.to_csv( os.path.join(out, '%s_all.txt'%(outstr[con])),
-                                sep='\t', float_format='%.8e', index=False, header=False)
 
-                    #handle the localizer case
+                    for con in events.trial_type.unique():
+                        con_timing = events[events.trial_type == con][['onset','duration']]
+                        con_timing['PM'] = 1
+                        con_timing.to_csv( os.path.join(out, '%s_all.txt'%(outstr[con])),
+                            sep='\t', float_format='%.8e', index=False, header=False)
+
                     #handle early/late for associative learning phases
-                    
 
 
