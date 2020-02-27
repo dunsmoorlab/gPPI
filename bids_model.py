8a5b1873a3794eea0e773118b75be62092bb5b4c
achennings <achennings@utexas.edu>
2020-02-27 11:19:56 -0600

    automated commit

--- bids_model.py
+++ bids_model.py
@@ -22,7 +22,7 @@ class bids_events():
         # self.confounds()
     
     def phase_events(self,phase):
-        ses_ = tasks[task]['ses']
+        ses_ = tasks[phase]['ses']
         return pd.read_csv(os.path.join(self.subj.subj_dir,'ses-'+str(ses_),'func',self.subj.fsub+'_ses-'+str(ses_)+'_task-'+phase+'_events.tsv'),sep='\t')
 
     #generate CS+, CS-, US timing files for use in FSL
