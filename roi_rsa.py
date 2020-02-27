80b408ab083cec03096921b039aa209cbbfe6751
achennings <achennings@utexas.edu>
2020-02-27 12:10:53 -0600

    automated commit

--- roi_rsa.py
+++ roi_rsa.py
@@ -121,7 +121,7 @@ class roi_rsa():
         foil_mask = self.mem_labels.memory_condition.isin(['Old'])
         self.mem_labels = self.mem_labels[foil_mask].reset_index(drop=True)
         self.mem_data = np.concatenate([self.mem_data['memory_run-01'],self.mem_data['memory_run-02'],self.mem_data['memory_run-03']],axis=-1)
-        self.mem_data = [:,:,:foil_mask]
+        self.mem_data = self.mem_data[:,:,:foil_mask]
 
 
         #we need this for the roi bootstrapping, probably best to do it here and broadcast
