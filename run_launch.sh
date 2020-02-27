2c5198214c9beb03848d90f64864f59354d7bd88
achennings <achennings@utexas.edu>
2020-02-26 20:53:05 -0600

    automated commit

--- run_launch.sh
+++ run_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-launch -N 48 -n 1 -J gPPI -s jobs/acq_CS_gPPI_job.txt -m achennings@utexas.edu -p normal -r 12:00:00 -A LewPea_MRI_Analysis
+launch -N 48 -n 48 -J gPPI -s jobs/acq_CS_gPPI_job.txt -m achennings@utexas.edu -p normal -r 12:00:00 -A LewPea_MRI_Analysis
