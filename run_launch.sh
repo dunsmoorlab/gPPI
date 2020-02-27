aa070ccac063eaa1d2a38b3a31fa41740176d165
achennings <achennings@utexas.edu>
2020-02-26 20:49:55 -0600

    automated commit

--- run_launch.sh
+++ run_launch.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-#for subI in 01 02 03 04 05 06 07 08 09 10 12 13 14 15 16 17 18 19 20 21
-#do
-#	launch -N 3 -n 3 -J er1_${subI} -s er_Sub0${subI}.txt -m achennings@utexas.edu -p normal -r 6:00:00 -A LewPea_MRI_Analysis
-#done
-
-#launch -N 20 -n 20 -J er_reg -s er_reg.txt -m achennings@utexas.edu -p normal -r 12:00:00 -A LewPea_MRI_Analysis
-
-
 
 launch -N 48 -n 1 -J gPPI -s jobs/acq_CS_gPPI_job.txt -m achennings@utexas.edu -p normal -r 12:00:00 -A LewPea_MRI_Analysis
