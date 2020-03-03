0d42bcd50f3c90f3054a50ecbfbf22871c657e56
achennings <achennings@utexas.edu>
2020-03-03 09:26:12 -0600

    automated commit

--- run_launch.sh
+++ run_launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-launch -N 1 -n 1 -J a3 -s jobs/acq_lvl3.txt -m achennings@utexas.edu -p normal -r 5:00:00 -A LewPea_MRI_Analysis
+launch -N 1 -n 1 -J a3 -s jobs/hc_seg_job.txt -m achennings@utexas.edu -p normal -r 12:00:00 -A LewPea_MRI_Analysis
