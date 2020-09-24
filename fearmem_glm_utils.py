7f5758444ae67a0334dfb9e6a74b5720ce93a1d0
achennings <achennings@utexas.edu>
2020-09-24 18:29:28 -0500

    dataTable

--- fearmem_glm_utils.py
+++ fearmem_glm_utils.py
@@ -635,10 +635,10 @@ def basic_model_dataTable():
                     data_table = data_table.append({
                         'Subj':sub,
                         'Sess':mem_phase,
-                        'Group':'healthy' if sub < 100 else 'ptss'
+                        'Group':'healthy' if sub < 100 else 'ptss',
                         'Encode':encode_phase,
                         'Condition':con,
                         'InputFile':f'/scratch/05426/ach3377/fc-bids/derivatives/model/{subj.fsub}/{mem_phase}/basic_model.feat/reg_standard/stats/cope{basic_pe_map[encode_phase][con]}.nii.gz'},
                         ignore_index=True)
-
+    data_table.to_csv('sm_events/memory_no_foils_dataTable.txt',index=False,sep=' ')
 
