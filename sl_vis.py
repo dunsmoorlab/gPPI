0ac437f28eba4c66ee0c7e1f5f0b07eed80e3876
achennings <achennings@utexas.edu>
2021-01-19 14:45:01 -0600

    mac

--- sl_vis.py
+++ sl_vis.py
@@ -54,3 +54,22 @@ seed in left angular gyrus. Let's plot that seed.
 """
 seed_coords = (-45, -67, 36)
 brain.add_foci(seed_coords, map_surface="white", hemi='lh')
+
+
+'''
+a priori rois for paper
+'''
+mri_file = f'{HOME}/Desktop/standard/vmPFC_mask.nii.gz'
+surf_data_lh = project_volume_data(mri_file, "lh", reg_file, projarg=[0, 1, .5], smooth_fwhm=1)
+surf_data_rh = project_volume_data(mri_file, "rh", reg_file, projarg=[0, 1, .5], smooth_fwhm=1)
+minval = np.min([np.min(surf_data_lh[np.nonzero(surf_data_lh)]),np.min(surf_data_rh[np.nonzero(surf_data_rh)])])
+    
+for view in ['med','lat']:
+    brain = Brain('fsaverage', 'split', surf, cortex='low_contrast',size=(400,400),
+                    views=view, background='white', foreground=None)
+    
+    brain.add_data(surf_data_lh, 0, .5, center=None, hemi='lh', thresh=minval, colorbar=False, colormap=cmap)
+    brain.add_data(surf_data_rh, 0, .5, center=None, hemi='rh',thresh=minval, colorbar=False, colormap=cmap)
+        
+    dACC_coords = (1, 21, 27)
+    brain.add_foci(dACC_coords, map_surface='pial', hemi='rh',color='orange')
